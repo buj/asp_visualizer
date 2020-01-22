511fd3fa155cb426760adfc0115a8d42c0437d6e
Truc Lam Bui <bujtuclam@gmail.com>
2020-01-22 13:53:14 +0100

    nodes/subgraphs merged into 'points', + tags, + nullary terms, + graph attributes, closureContradiction

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ from visual import *
 
 
 stylesheet1 = {
+  "closureContradiction": {
+    "shape": "plaintext",
+    "fontcolor": "red"
+  },
   "mev": {
     "shape": "rectangle"
   },
@@ -24,41 +28,50 @@ stylesheet1 = {
     "color": "red",
     "style": "bold",
     "arrowhead": "vee",
-    "arrowsize": 0.5
+    "arrowsize": 1
   },
   "commits": {
     "label": "",
     "color": "red",
     "arrowhead": "vee",
-    "arrowsize": 0.5
+    "arrowsize": 1
   },
   "mustHappenBefore": {
     "label": "",
     "style": "bold",
     "arrowhead": "vee",
-    "arrowsize": 0.5
+    "arrowsize": 1
   },
-  "readsFromTarget": {
+  "exportsToTarget": {
     "label": "",
     "color": "green",
     "arrowhead": "vee",
-    "arrowsize": 0.5,
-    "dir": "back"
+    "arrowsize": 1
   },
-  "derivedHappensBefore": {
+  "coolHappensBefore": {
     "label": "",
     "style": "dashed",
     "arrowhead": "vee",
-    "arrowsize": 0.5
+    "arrowsize": 1
+  },
+  "auxThread": {
+    "color": "lightgray",
+    "style": "filled"
   }
 }
+
+def dlv_strip(line):
+  if line[:5] == "Cost:" or line[:3] == "DLV":
+    return ""
+  line = line.lstrip("Best model: ")
+  return line
 
 def from_file(fname):
   res = []
   with open(fname, "r") as fin:
     for line in fin:
-      line.strip()
-      if len(line) == 0 or line[:3] == "DLV":
+      line = dlv_strip(line)
+      if line == "":
         continue
       res += [line]
   return '\n'.join(res)
@@ -68,11 +81,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input_file", type=str)
 parser.add_argument("output_file", type=str)
 parser.add_argument("--viz", type=str, default="dot", help="which graphviz to use")
-args = parser.parse_args()
+args, g_args = parser.parse_known_args()
 
 inp = from_file(args.input_file)
 vkb = VisualKB(Parser(inp).parse_kb())
 vkb.add_formats(**stylesheet1)
+for g_arg in g_args:
+  ls = g_arg.strip().split('=')
+  assert len(ls) == 2, "Graph attribute arguments should have format 'key=val'"
+  vkb.set_g_attr(ls[0], ls[1])
 
 cmd = ' '.join([args.viz, "-Tpng", "-o", args.output_file])
 logging.info("Executing command:", cmd)
--- visual.py
+++ visual.py
@@ -18,11 +18,30 @@ class WithId:
     res = cls.free_id
     cls.free_id += 1
     return res
+  
+  def __init__(self):
+    super().__init__()
+    self.id = self.next_id()
+
+
+class WithTags:
+  def __init__(self, *tags):
+    super().__init__()
+    self.tags = set(tags)
+  
+  def add_tag(self, tag):
+    if tag not in self.tags:
+      self.tags.add(tag)
+      return True
+  
+  def has_tag(self, tag):
+    return tag in self.tags
 
 
 class WithAttrs:
-  def __init__(self, **kwargs):
-    self.attrs = kwargs
+  def __init__(self):
+    super().__init__()
+    self.attrs = {}
   
   def clear_attrs(self):
     self.attrs.clear()
@@ -41,32 +60,114 @@ class WithAttrs:
       yield f"{key} = {viz_repr(val)}"
 
 
-class Node(WithAttrs):
+class WithChildren:
+  def __init__(self):
+    super().__init__()
+    self.children = {}
+  
+  def __getitem__(self, key):
+    return self.children[str(key)]
+  
+  @property
+  def has_child(self):
+    return len(self.children) > 0
+  
+  def add_child(self, child):
+    assert isinstance(child, WithParent), "Cannot add non-WithParent as a child"
+    name = child.name
+    if name not in self.children:
+      child.set_parent(self)
+      self.children[name] = child
+      return True
+
+
+class WithParent:
+  def __init__(self):
+    super().__init__()
+    self.parent = None
+  
+  @property
+  def has_parent(self):
+    return self.parent is not None
+  
+  def set_parent(self, par):
+    assert self.parent is None, "Cannot change parent once set"
+    assert isinstance(par, WithChildren), "Cannot set parent to a non-WithChildren"
+    self.parent = par
+
+
+class Point(WithId, WithAttrs, WithTags, WithParent, WithChildren):
   def __init__(self, term, **kwargs):
-    WithAttrs.__init__(self, **kwargs)
+    super().__init__()
+    self.update_attrs(**kwargs)
     self.term = term
-    self.name = viz_repr(str(self.term))
+  
+  @property
+  def name(self):
+    return str(self.term)
+  
+  @property
+  def repr_name(self):
+    return viz_repr(self.name)
+  
+  @property
+  def is_sg(self):
+    return self.has_child or self.has_tag("cluster")
+  
+  @property
+  def sg_name(self):
+    res = ""
+    res += ("cluster_" if self.has_tag("cluster") else "")
+    res += ("sg_" if self.has_child else "")
+    res += self.name
+    return viz_repr(res)
   
   @property
   def t(self):
     return self.term.fname
   
+  @property
+  def lines(self):
+    if self.is_sg:
+      lines = []
+      lines += [f"subgraph {self.sg_name} {{"]
+      for s in self.attr_strs():
+        lines += [s]
+      lines += [f'{self.repr_name} [shape = "plaintext"]']
+      for ch in self.children.values():
+        lines += ch.lines
+      lines += ["}"]
+      return lines
+    return [f"{self.repr_name} [{', '.join(self.attr_strs())}]"]
+  
   def __str__(self):
-    return f"{self.name} [{', '.join(self.attr_strs())}]"
+    return '\n'.join(self.lines)
 
 
-class Edge(WithId, WithAttrs):
+class Edge(WithId, WithAttrs, WithTags):
   def __init__(self, t, src, dest, **kwargs):
     if "label" not in kwargs:
       kwargs["label"] = t
-    WithAttrs.__init__(self, **kwargs)
-    self.name = f"edge({t}){self.next_id()}"
+    super().__init__()
+    self.update_attrs(**kwargs)
     self.t = t
     self.src = src
     self.dest = dest
   
+  @property
+  def name(self):
+    return f"{self.t}({self.src.name}, {self.dest.name})"
+  
+  def show(self):
+    return self.name
+  
   def __str__(self):
-    return f'{self.src.name} -> {self.dest.name} [{", ".join(self.attr_strs())}]'
+    ltail = ([f"ltail = {self.src.sg_name}"] if self.src.is_sg else [])
+    lhead = ([f"lhead = {self.dest.sg_name}"] if self.dest.is_sg else [])
+    if self.src.sg_name == self.dest.sg_name:
+      ltail, lhead = [], []
+    attrs = lhead + ltail + list(self.attr_strs())
+    return f'{self.src.repr_name} -> {self.dest.repr_name} [{", ".join(attrs)}]'
 
 
 def interpret_term(term):
@@ -80,49 +181,81 @@ def interpret_term(term):
     return float(str(term.subs[0]))
   return str(term)
 
+
 class VisualKB:
-  def __init__(self, kb=KB()):
-    self.nodes = {}
+  def __init__(self, kb=KB(), **kwargs):
+    self.points = {}  # nodes/subgraphs
     self.edges = {}
     self.formatting = {}
     self.fterms = []
+    if "compound" not in kwargs:
+      kwargs["compound"] = "true"
+    self.g_attrs = kwargs
     for term in kb:
       self.add(term)
   
-  def declare_node(self, term):
+  def set_g_attr(self, key, val):
+    self.g_attrs[key] = val
+  
+  def declare_point(self, term):
     name = str(term)
-    if name not in self.nodes:
-      self.nodes[name] = Node(term)
+    if name not in self.points:
+      self.points[name] = Point(term)
       return True
   
-  def node(self, term):
-    self.declare_node(term)
-    return self.nodes[str(term)]
+  def point(self, term):
+    self.declare_point(term)
+    return self.points[str(term)]
+  
+  def add(self, term):
+    if term.arity == 0:
+      self.declare_point(term)
+      return True
+    elif term.arity == 1:
+      if term.fname == "show":
+        return self.add(term.subs[0])
+      point = self.point(term.subs[0])
+      return point.add_tag(term.fname)
+    elif term.arity == 2:
+      src = self.point(term.subs[0])
+      dest = self.point(term.subs[1])
+      if term.fname == "contains":
+        src.add_child(dest)
+        return True
+      else:
+        e = Edge(term.fname, src, dest)
+        assert e.name not in self.edges, "Edge already present"
+        self.edges[e.name] = e
+        return True
+    elif term.arity == 3:
+      if term.fname == "attr":
+        self.fterms.append(term)
+        return True
   
   def apply_format(self):
     """Applies formatting to all nodes and edges. Node are identified
     based on their tags (if there are multiple choices, the result is
     the sum of attributes); edges are identified based on their fname (type)."""
-    for node in self.nodes.values():
-      node.clear_attrs()
+    for point in self.points.values():
+      point.clear_attrs()
     for edge in self.edges.values():
       edge.clear_attrs()
     for key, fmt in self.formatting.items():
-      for node in self.nodes.values():
-        if key != node.t:
+      for point in self.points.values():
+        if key != point.t and not point.has_tag(key):
           continue
-        node.update_attrs(**fmt)
+        point.update_attrs(**fmt)
       for edge in self.edges.values():
-        if key != edge.t:
+        if key != edge.t and not edge.has_tag(key):
           continue
         edge.update_attrs(**fmt)
     for term in self.fterms:
       if term.fname == "attr":
         assert len(term.subs) == 3, "attr term must have arity 3 (node, attrname, val)"
-        node = self.node(term.subs[0])
+        point = self.point(term.subs[0])
         key = str(term.subs[1])
         val = interpret_term(term.subs[2])
-        node[key] = val
+        point[key] = val
   
   def add_format(self, fname, attrs):
     self.formatting[fname] = attrs
@@ -136,26 +269,17 @@ class VisualKB:
       del self.formatting[fname]
       return True
   
-  def add(self, term):
-    if term.arity == 1:
-      subterm = term.subs[0]
-      return self.declare_node(subterm)
-    if term.arity == 2:
-      src = self.node(term.subs[0])
-      dest = self.node(term.subs[1])
-      e = Edge(term.fname, src, dest)
-      self.edges[e.name] = e
-      return True
-    if term.arity == 3 and term.fname == "attr":
-      self.fterms.append(term)
-  
   def __str__(self):
     """Output in graphviz format."""
     self.apply_format()
     fout = io.StringIO()
     print("digraph {", file=fout)
-    for node in self.nodes.values():
-      print(str(node), file=fout)
+    for key, val in self.g_attrs.items():
+      print(key, "=", val, file=fout)
+    for point in self.points.values():
+      if point.has_parent:
+        continue
+      print(str(point), file=fout)
     for edge in self.edges.values():
       print(str(edge), file=fout)
     print("}", file=fout)
